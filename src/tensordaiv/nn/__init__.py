33504dae5d75d17b9e94ec630cfbab5095c8ae14
BREW <IRACK000@gmail.com>
2024-05-21 02:45:47 +0900

    Fix: Model Name Override

--- src/tensordaiv/nn/__init__.py
+++ src/tensordaiv/nn/__init__.py
@@ -1,12 +1,21 @@
 from keras import *
+from keras.src.utils.naming import auto_name
 
 
 class Model:
     def __new__(cls, *args, **kwargs):
         self = super().__new__(cls)
         self.__init__(*args, **kwargs)
+
         input_example = Input(shape=self.input_size)
-        return models.Model(inputs=input_example, outputs=self.call(input_example))
+        model = models.Model(inputs=input_example, outputs=self.call(input_example), name=auto_name(cls.__name__))
+
+        model.__dict__['__name__'] = cls.__name__
+        model.__dict__['__module__'] = cls.__module__
+        model.__dict__['__qualname__'] = cls.__qualname__
+        model.__dict__['__doc__'] = cls.__doc__
+
+        return model
 
     def __init__(self, input_size, *args, **kwargs):
         self.input_size = input_size
